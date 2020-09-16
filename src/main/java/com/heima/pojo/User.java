93b62c475f7c210d94dc64b3a697291c91c46b80
javalwl <https://gitee.com/ javalwl>
2020-09-16 17:10:49 +0800

    firstcommit

---
+++ src/main/java/com/heima/pojo/User.java
@@ -0,0 +1,14 @@
+package com.heima.pojo;
+
+import lombok.Data;
+
+/**
+ * 2 * @Author: liwanlei
+ * 3 * @Date: 2020/9/15 21:00
+ * 4
+ */
+@Data
+public class User {
+    private  String name;
+    private  String email;
+}
